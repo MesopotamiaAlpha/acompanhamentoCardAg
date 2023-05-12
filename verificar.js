9ea932daa1b1a1111cedb24d9acdd83ceb4f5f9d
Jose Antonio <leao-gomes@hotmail.com>
2023-05-12 20:42:00 -0300

    Adição do venom bot e disparo de mensagem via what

--- verificar.js
+++ verificar.js
@@ -1,6 +1,8 @@
 const { exec } = require('child_process');
 const fs = require('fs');
 const Listr = require('listr');
+const venom = require('venom-bot');
+const qrcode = require('qrcode-terminal');
 
 const tasks = new Listr([
   {
@@ -25,13 +27,60 @@ const tasks = new Listr([
   }
 ]);
 
-function run() {
+function run(client) {
   tasks.run().then(() => {
-    setTimeout(run, 10000); // Repete o teste após 10 segundos
+    setTimeout(() => {
+      run(client);
+    }, 10000); // Repete o teste após 10 segundos
   }).catch(() => {
-    setTimeout(run, 0); // Repete o teste imediatamente em caso de erro
+    const message = 'Perfil alterado, acione o responsável';
+    const numbers = ['5512981006992', '5512981006992', '5512981006992', '5512981006992'];
+    sendMessage(client, numbers);
+    setTimeout(() => {
+      run(client);
+    }, 0); // Repete o teste imediatamente em caso de erro
   });
 }
 
-console.log('Iniciando programa em loop');
-run();
+
+function sendMessage(client, numbers) {
+  const message = 'Perfil alterado, acione o responsável';
+  numbers.forEach((number) => {
+    if (client && client.sendText) {
+      client.sendText(`${number}@c.us`, message)
+        .then(() => console.log(`Mensagem enviada para os responsaveis`))
+        .catch((error) => console.error(`Erro ao enviar mensagem para ${number}:`, error));
+    } else {
+      console.error('Erro: cliente não está definido ou não tem o método sendText');
+    }
+  });
+}
+
+venom
+  .create()
+  .then((client) => {
+    const cliente = client;
+    console.log('Iniciando programa em loop');
+    run(client);
+    client.onStreamChange((state) => {
+      console.log('Estado do cliente:', state);
+    });
+
+    client.onReady(() => {
+      console.log('Cliente iniciado com sucesso!');
+      qrcode.generate(client.base64QrCode, { small: true });
+    });
+
+    client.onIncomingCall((call) => {
+      client.sendText(call.peerJid, 'Desculpe, não é possível atender ligações.');
+    });
+
+    client.onMessage((message) => {
+      if (message.body === 'ping') {
+        client.sendText(message.from, 'pong');
+      }
+    });
+  })
+  .catch((error) => {
+    console.error('Erro ao iniciar cliente:', error);
+  });
